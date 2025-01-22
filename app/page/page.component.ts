5cf06f79d657be719db49083a9cfb74bf6b98cf3
esavage <esavage@digitecinteractive.com>
2025-01-22 10:34:09 -0600

    add window reference to pages

--- app/page/page.component.ts
+++ app/page/page.component.ts
@@ -6,6 +6,7 @@ import { CoreService } from '@tetra/core.service';
 import { AppService } from '@tetra/app.service';
 import { UserService } from '@tetra/user.service';
 import { User } from '@tetra/user';
+import { WindowRefService, ICustomWindow } from '@tetra/window.service'
 
 @Component({
   selector: 'app-page',
@@ -22,14 +23,17 @@ export class TetraPage {
     showHeader: true,
     showTitle: true
   };
+  protected _window: ICustomWindow;
 
   constructor(
     protected core: CoreService,
     protected app: AppService,
     protected route: Router,
     protected activeRoute: ActivatedRoute,
-    protected userService: UserService
+    protected userService: UserService,
+    protected windowRef: WindowRefService
   ) {
+    this._window = windowRef.nativeWindow;
     userService.getUser().subscribe((user: User | null) => {
       if (user) {
         this.user = user;
