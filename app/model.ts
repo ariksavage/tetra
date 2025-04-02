d3688f81bfd6231e8483b45f6c7140af22e9c713
esavage <esavage@digitecinteractive.com>
2025-04-02 12:34:08 -0500

    date format methods

--- app/model.ts
+++ app/model.ts
@@ -2,10 +2,10 @@ import { DateFormat } from '@tetra/date-format';
 
 export class Model {
   id!: number;
-  date_created!: Date;
-  created_by!: number;
-  date_modified!: Date;
-  modified_by!: number;
+  date_created: Date = new Date();
+  created_by: number = 0;
+  date_modified: Date = new Date();
+  modified_by: number = 0;
 
   constructor(data: any) {
     if (data) {
@@ -23,10 +23,68 @@ export class Model {
   }
 
   protected formatDate(format: string, dateStr: string) {
+    if (!dateStr) {
+      return '';
+    }
     const date = new DateFormat(format, dateStr);
     return date.string();
   }
   protected mapData(data: any) {
     Object.assign(this, data);
+  }
+
+  public dateCreated() {
+    return this.formatDate('M j, Y', this.date_created.toString());
+  }
+
+  public sinceCreated() {
+    return this.since(this.date_created).replace('approximately','').trim();
+  }
+
+  public dateModified() {
+    return this.formatDate('M j, Y', this.date_modified.toString());
+  }
+
+  public sinceModified() {
+    return this.since(this.date_modified).replace('approximately','').trim();
+  }
+
+  public since(date: Date) {
+    if (!date) {
+      return '';
+    }
+    let str = '';
+    let n = 0;
+    const msSecond = 1000;
+    const msMinute = 60 * msSecond;
+    const msHour = 60 * msMinute;
+    const msDay = 24 * msHour;
+    const msMonth = 30 * msDay;
+    const msYear = 365 * msDay;
+
+    const elapsed = new Date().getTime() - date.getTime();
+
+    if (elapsed < msMinute) {
+      n = Math.round(elapsed / msSecond);
+      str = n + ' second';
+    } else if (elapsed < msHour) {
+      n = Math.round(elapsed / msMinute);
+      str = n + ' minute';
+    } else if (elapsed < msDay ) {
+      n = Math.round(elapsed / msHour);
+      str = n + ' hour';
+    } else if (elapsed < msMonth) {
+      n = Math.round(elapsed / msDay);
+      str = 'approximately ' + n + ' day';
+    } else if (elapsed < msYear) {
+      n = Math.round(elapsed / msMonth);
+      str = 'approximately ' + n + ' month';
+    } else {
+      n = Math.round(elapsed / msYear);
+      str = 'approximately ' + n + ' year';
+    }
+    str += n == 1 ? ' ago' : 's ago';
+
+    return str;
   }
 }
