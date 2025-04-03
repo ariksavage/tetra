3b9c178883e41fed74bc60b6afa8a3da3b8a6e03
esavage <esavage@digitecinteractive.com>
2025-04-03 14:29:45 -0500

    add multi-select and tag support

--- app/field-multi-select/field-multi-select.component.ts
+++ app/field-multi-select/field-multi-select.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 // import { TetraFieldComponent } from '../field/field.component';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -10,18 +10,38 @@ import { CommonModule } from '@angular/common';
     templateUrl: './field-multi-select.component.html',
     styleUrl: './field-multi-select.component.scss'
 })
-export class TetraFieldMultiSelectComponent extends Component {
+export class TetraFieldMultiSelectComponent {
   @Input() label: string = '';
+  @Input() placeholder: string = '';
   @Input() model: any = null;
   @Output() modelChange: EventEmitter<any> = new EventEmitter<any>();
   @Input() items: Array<any> = [];
   @Input() titleFunc: any = null;
   @Input() newFunc: any = null;
   filter: string = '';
+  focus: boolean = false;
+  blurTO: any = null;
+
+  id(){
+    return 'multi-select-' + this.label.toLowerCase();
+  }
 
   filteredItems() {
-    return this.items.filter(x => {
-      return !this.model.includes(x) && (!this.filter || this.titleFunc(x).indexOf(this.filter) > -1);
+    return this.items.filter((item: any) => {
+      let include = true;
+      if (this.filter && this.titleFunc(item).indexOf(this.filter) == -1) {
+        include = false;
+      } else {
+        this.model.forEach((modelItem: any) => {
+          if (this.titleFunc(modelItem) == this.titleFunc(item)){
+            include = false;
+          }
+        })
+      }
+      return include;
+      // return !this.model.includes(x) && (!this.filter || this.titleFunc(x).indexOf(this.filter) > -1);
+    }).sort((a: any, b: any) => {
+      return this.titleFunc(a).localeCompare(this.titleFunc(b));
     });
   }
 
@@ -31,6 +51,18 @@ export class TetraFieldMultiSelectComponent extends Component {
     }
     let matches = this.items.filter(item => this.titleFunc(item) == this.filter);
     return matches.length > 0;
+  }
+
+  onFocus() {
+
+    clearTimeout(this.blurTO);
+    this.focus = true;
+  }
+
+  onBlur() {
+    this.blurTO = setTimeout(() => {
+      this.focus = false;
+    }, 10);
   }
 
   add(item: any){
