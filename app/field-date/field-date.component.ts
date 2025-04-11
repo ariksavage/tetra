6e2c12226486cf7ab21f723c2ab91bc77845383f
esavage <esavage@digitecinteractive.com>
2025-04-11 09:30:27 -0500

    improve date field

--- app/field-date/field-date.component.ts
+++ app/field-date/field-date.component.ts
@@ -1,17 +1,16 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { FieldComponent } from '../field/field.component';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+// import { TetraFieldComponent } from '@tetra/field/field.component';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
-		standalone: true,
+	standalone: true,
   selector: '.field.date',
-  standalone: true,
   imports: [ CommonModule, FormsModule ],
   templateUrl: './field-date.component.html',
   styleUrl: './field-date.component.scss'
 })
-export class TetraFieldDateComponent {
+export class TetraFieldDateComponent implements OnInit{
   @Input() label: string = '';
   @Input() model: any = null;
   month: number|null = null;
@@ -19,7 +18,7 @@ export class TetraFieldDateComponent {
   year: number|null = null;
   temp: string = '';
   @Input() placeholder: string = '';
-  @Output() modelChange = new EventEmitter<string>();
+  @Output() modelChange = new EventEmitter<any>();
   error: string = '';
 
   ngOnInit() {
@@ -28,23 +27,59 @@ export class TetraFieldDateComponent {
       this.month = d.getMonth() + 1;
       this.date = d.getDate();
       this.year = d.getFullYear();
+      this.daysInMonth();
+    } else {
+      setTimeout(() => {
+        this.ngOnInit()
+      }, 100);
     }
   }
 
+  daysInMonth() {
+    let d = 31;
+    if (this.month) {
+      switch(this.month.toString()) {
+        case '2': // February
+          if (this.year){
+            d = this.year % 4 == 0 ? 29 : 28;
+          } else {
+            d = 28;
+          }
+          break;
+        case '9':  // hath September
+        case '4':  // April
+        case '6':  // June
+        case '11': // and November
+          d = 30;
+          break;
+        default:
+          d = 31;
+          break;
+      }
+    }
+    if (this.date && this.date > d){
+      this.date = d;
+    }
+    return d;
+  }
+
   update() {
-    let d = '';
-    if (this.year){
-      d += this.year.toString();
-    }
-    d += '-';
-    if (this.month){
-      d += this.month.toString().padStart(2, '0');
-    }
-    d += '-';
-    if (this.date){
-      d += this.date.toString().padStart(2, '0');
-    }
-    d += 'T00:00:00';
-    this.modelChange.emit(d);
+    this.daysInMonth();
+    setTimeout(() =>{
+      let d = '';
+      if (this.year){
+        d += this.year.toString();
+      }
+      d += '-';
+      if (this.month){
+        d += this.month.toString().padStart(2, '0');
+      }
+      d += '-';
+      if (this.date){
+        d += this.date.toString().padStart(2, '0');
+      }
+      d += 'T00:00:00';
+      this.modelChange.emit(new Date(d).toUTCString());
+    }, 100);
   }
 }
